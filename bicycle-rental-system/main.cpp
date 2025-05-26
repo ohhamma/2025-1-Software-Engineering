ff4026c3b4c1f128700b47106c6ba4118a1eaa18
ohhamma <judyham10634@naver.com>
2025-05-27 06:17:57 +0900

    refactor: 코딩 컨벤션에 맞게 수정

--- bicycle-rental-system/main.cpp
+++ bicycle-rental-system/main.cpp
@@ -9,78 +9,77 @@
 
 int main() {
 
-    // 파일 입출력을 위한 초기화
-    std::ifstream in_fp(INPUT_FILE_NAME);
-    std::ofstream out_fp(OUTPUT_FILE_NAME);
-    std::string line;
+  // 파일 입출력을 위한 초기화
+  std::ifstream in_fp(INPUT_FILE_NAME);
+  std::ofstream out_fp(OUTPUT_FILE_NAME);
 
-    // 메뉴 입력값 초기화
-    int menu_level_1 = 0;
-    int menu_level_2 = 0;
-    bool is_program_exit = false;
+  // 메뉴 입력값 초기화
+  int menu_level_1 = 0;
+  int menu_level_2 = 0;
+  bool is_program_exit = false;
 
-    // 시스템 종료되기 전까지 실행
-    while (!is_program_exit) {
-        in_fp >> menu_level_1 >> menu_level_2;
+  // 시스템 종료되기 전까지 실행
+  while (!is_program_exit) {
+    in_fp >> menu_level_1 >> menu_level_2;
 
-        switch (menu_level_1) {
-        case 1:
-            switch (menu_level_2) {
-            case 1:
-                // 1.1. 회원가입
-                break;
-            }
-            break;
-
-        case 2:
-            switch (menu_level_2) {
-            case 1:
-                // 2.1. 로그인
-                break;
-            case 2:
-                // 2.2. 로그아웃
-                break;
-            }
-            break;
-
-        case 3:
-            switch (menu_level_2) {
-            case 1:
-                // 3.1. 자전거 등록
-                break;
-            }
-            break;
-
-        case 4:
-            switch (menu_level_2) {
-            case 1:
-                // 4.1. 자전거 대여
-                break;
-            }
-            break;
-
-        case 5:
-            switch (menu_level_2) {
-            case 1:
-                // 5.1. 자전거 대여 리스트
-                break;
-            }
-            break;
-
-        case 6:
-            switch (menu_level_2) {
-            case 1:
-                // 6.1. 종료
-                is_program_exit = true;
-                break;
-            }
+    switch (menu_level_1) {
+      case 1:
+        switch (menu_level_2) {
+          case 1:
+            // 1.1. 회원가입
             break;
         }
+        break;
+
+      case 2:
+        switch (menu_level_2) {
+          case 1:
+            // 2.1. 로그인
+            break;
+          case 2:
+            // 2.2. 로그아웃
+            break;
+        }
+        break;
+
+      case 3:
+        switch (menu_level_2) {
+          case 1:
+            // 3.1. 자전거 등록
+            break;
+        }
+        break;
+
+      case 4:
+        switch (menu_level_2) {
+          case 1:
+            // 4.1. 자전거 대여
+            break;
+        }
+        break;
+
+      case 5:
+        switch (menu_level_2) {
+          case 1:
+            // 5.1. 자전거 대여 리스트
+            break;
+        }
+        break;
+
+      case 6:
+        switch (menu_level_2) {
+          case 1:
+            // 6.1. 종료
+            is_program_exit = true;
+            break;
+        }
+        break;
     }
+  }
 
-    // 파일 닫기
-    in_fp.close();
-    out_fp.close();
+  // 파일 닫기
+  in_fp.close();
+  out_fp.close();
 
-    return 0;
-} 
+  return 0;
+}
