39a5f28cba5d07a85287eb81d5ef550448272231
ohhamma <judyham10634@naver.com>
2025-05-27 07:32:06 +0900

    refactor: loginUser가 User를 반환하도록 수정

--- bicycle-rental-system/Login.cpp
+++ bicycle-rental-system/Login.cpp
@@ -4,17 +4,17 @@
 Login::Login(UserCollection* user_collection)
     : user_collection_(user_collection) {}
 
-void Login::loginUser(const std::string& id,
-                      const std::string& password) {
+User* Login::loginUser(const std::string& id,
+                       const std::string& password) {
   User* user = user_collection_->findUserById(id);
 
   if (user == nullptr) {
-    std::cout << "로그인 실패: 아이디가 존재하지 않습니다.\n";
-    return;
+    return nullptr;
   }
 
   if (!user->checkPassword(password)) {
-    std::cout << "로그인 실패: 비밀번호가 틀렸습니다.\n";
-    return;
+    return nullptr;
   }
+
+  return user;  // 로그인 성공
 }
--- bicycle-rental-system/Login.h
+++ bicycle-rental-system/Login.h
@@ -7,8 +7,8 @@ class Login {
  public:
   Login(UserCollection* user_collection);
 
-  void loginUser(const std::string& id,
-                    const std::string& password);
+  User* loginUser(const std::string& id,
+                  const std::string& password);
 
  private:
   UserCollection* user_collection_;
