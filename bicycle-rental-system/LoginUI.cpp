af023c6f3e1c11b2025ccc62ee5bf6c3989b8e54
ohhamma <judyham10634@naver.com>
2025-05-27 07:28:18 +0900

    feat: LoginUI 구현

---
+++ bicycle-rental-system/LoginUI.cpp
@@ -0,0 +1,18 @@
+#include "LoginUI.h"
+
+LoginUI::LoginUI(std::ifstream& in_fp, std::ofstream& out_fp, Login* login)
+    : in_fp_(in_fp), out_fp_(out_fp), login_(login) {}
+
+void LoginUI::login() {
+  std::string id, password;
+
+  // 입력 형식: ID와 Password를 파일로부터 읽음
+  in_fp_ >> id >> password;
+
+  // 로그인 시도
+  login_->loginUser(id, password);
+
+  // 출력 형식
+  out_fp_ << "2.1. 로그인\n";
+  out_fp_ << "> " << id << " " << password << "\n";
+}
