0ba4312679529b4eed2f36918eb345aa05ed1cda
ohhamma <judyham10634@naver.com>
2025-05-27 10:02:28 +0900

    fix: 주어진 형식에 맞게 id만 입력받도록 수정

--- bicycle-rental-system/RentBicycleUI.cpp
+++ bicycle-rental-system/RentBicycleUI.cpp
@@ -6,21 +6,21 @@ RentBicycleUI::RentBicycleUI(std::ifstream& in_fp, std::ofstream& out_fp,
       login_(login), rent_bicycle_(rent_bicycle) {}
 
 void RentBicycleUI::rentBicycle() {
-  std::string id, name;
+  std::string id;
 
-  // 입력 형식: 자전거 ID, 자전거 제품명을 파일로부터 읽음
-  in_fp_ >> id >> name;
+  // 입력 형식: 자전거 ID를 파일로부터 읽음
+  in_fp_ >> id;
 
   // 현재 로그인되어 있는 사용자 조회
   User* current_user = login_->getCurrentUser();
 
   if (current_user != nullptr) {
     // 자전거 대여
-    rent_bicycle_->rentBicycle(id, current_user);
+    Bicycle *bicycle = rent_bicycle_->rentBicycle(id, current_user);
 
     // 출력 형식
     out_fp_ << "4.1. 자전거 대여\n";
-    out_fp_ << "> " << id << " " << name << "\n";
+    out_fp_ << "> " << bicycle->getId() << " " << bicycle->getName() << "\n";
     out_fp_ << "\n";
   }
 }
